ae3d7afc62b37b3c3a577dd2a5b0ecfda3edff5a
mkarsums15@mail.ru <mkarsums15@mail.ru>
2022-12-26 18:06:44 +0300

    added hamburger menu to homepage, added media queries to max-width: 768px

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,5 @@
+// Раскрывающийся блок в секции "Вопрос-ответ"
+
 let plus = document.querySelectorAll(".questions__plus");
 let minus = document.querySelectorAll(".questions__minus");
 let answer = document.querySelectorAll(".questions__answer");
@@ -16,3 +18,23 @@ for (let i = 0; i < plus.length; i++) {
 		minus[i].style.display = "none";
 	});
 }
+
+// Гамбургер-меню
+
+window.addEventListener("DOMContentLoaded", () => {
+	let menu = document.querySelector(".header__menu"),
+		menuItem = document.querySelectorAll(".header__link"),
+		hamburger = document.querySelector(".hamburger");
+
+	hamburger.addEventListener("click", () => {
+		hamburger.classList.toggle("hamburger_active");
+		menu.classList.toggle("header__menu_active");
+	});
+
+	menuItem.forEach((item) => {
+		item.addEventListener("click", () => {
+			hamburger.classList.toggle("hamburger_active");
+			menu.classList.toggle("header__menu_active");
+		});
+	});
+});
